e9adf66605c65474cd3d2d77476d6920e932a8bb
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2022-12-05 12:54:43 +0100

    Day 5

---
+++ 2022/05/two.php
@@ -0,0 +1,61 @@
+<?php
+
+function load(string $filename): array
+{
+    $data = file($filename);
+    $split = array_search(PHP_EOL, $data);
+
+    return [
+        parseStack(array_reverse(array_slice($data, 0, $split - 1))),
+        array_slice($data, $split + 1),
+    ];
+}
+
+/**
+ * in:
+ *      [D]
+ *  [N] [C]
+ *  [Z] [M] [P]
+ *   1   2   3
+ *
+ * out:
+ *  1: ['Z', 'N']
+ *  2: ['M', 'C', 'D']
+ *  3: ['P']
+ */
+function parseStack(array $input): array
+{
+    $stack = [];
+    foreach ($input as $item) {
+        foreach (str_split($item, 4) as $index => $value) {
+            if ($val = preg_replace('/[^A-Z]/', '', $value)) {
+                $stack[$index+1] = $stack[$index+1] ?? [];
+                array_push($stack[$index+1], $val);
+            }
+        }
+    }
+    return $stack;
+}
+
+function handle(string $filename): string
+{
+    [$stack, $instructions] = load($filename);
+    foreach ($instructions as $instruction) {
+        preg_match('/^move (\d+) from (\d+) to (\d+)\s$/', $instruction, $matches);
+        [, $count, $from, $to] = $matches;
+        $popped = [];
+        do {
+            array_push($popped, array_pop($stack[$from]));
+        } while (--$count > 0);
+
+        array_push($stack[$to], ... array_reverse($popped));
+    }
+
+    return join('', array_map(fn(array $in) => array_pop($in), $stack));
+}
+
+$testScore = handle('input-test');
+assert('MCD' === $testScore, sprintf('Expected MCD, got [%d] instead.', $testScore));
+var_dump('Test passed!');
+
+var_dump(handle('input'));
