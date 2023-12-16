606c0c657c03d076e8d5ec92c167e762dddb6585
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2023-12-16 12:25:15 +0100

    2023 day 16
    ```
    > python3 day16.py
    Day 16, Part 1: 7074  in 0.007695198059082031 seconds
    Day 16, Part 2: 7530  in 0.842634916305542 seconds
    ```

---
+++ 2023/day16.py
@@ -0,0 +1,118 @@
+import time
+
+
+class D:
+    up = 0
+    right = 1
+    down = 2
+    left = 3
+
+
+directions = [
+    (-1, 0),  # up
+    (0, 1),  # right
+    (1, 0),  # down
+    (0, -1),  # left
+]
+
+
+def part1(filename):
+    return visit(get_grid(filename), (0, 0), D.right)
+
+
+def part2(filename):
+    grid = get_grid(filename)
+    score = 0
+    for i in range(len(grid)):
+        score = max(score, visit(grid, (0, i), D.down))
+        score = max(score, visit(grid, (len(grid), i), D.up))
+        score = max(score, visit(grid, (i, 0), D.right))
+        score = max(score, visit(grid, (i, len(grid)), D.left))
+    return score
+
+
+def visit(grid, start_pos, start_dir):
+    queue = [(start_pos, start_dir, set())]
+    visited = set()
+
+    while len(queue) > 0:
+        current_pos, current_dir, seen = queue.pop(0)
+        if not is_in_grid(grid, current_pos):
+            continue
+        if (current_pos, current_dir) in seen:
+            continue
+
+        visited.add(current_pos)
+        seen.add((current_pos, current_dir))
+
+        current_item = grid[current_pos[0]][current_pos[1]]
+
+        next_directions = []
+        if current_item == '.':
+            next_directions = [current_dir]
+        elif current_item == '|':
+            if current_dir in [D.left, D.right]:
+                next_directions = [D.up, D.down]
+            else:
+                next_directions = [current_dir]
+        elif current_item == '-':
+            if current_dir in [D.up, D.down]:
+                next_directions = [D.left, D.right]
+            else:
+                next_directions = [current_dir]
+        elif current_item == '/':
+            match current_dir:
+                case D.up:
+                    next_directions = [D.right]
+                case D.right:
+                    next_directions = [D.up]
+                case D.down:
+                    next_directions = [D.left]
+                case D.left:
+                    next_directions = [D.down]
+                case _:
+                    assert False
+
+        elif current_item == 'b':
+            match current_dir:
+                case D.up:
+                    next_directions = [D.left]
+                case D.right:
+                    next_directions = [D.down]
+                case D.down:
+                    next_directions = [D.right]
+                case D.left:
+                    next_directions = [D.up]
+                case _:
+                    assert False
+
+        for next_direction in next_directions:
+            next_position = (
+            current_pos[0] + directions[next_direction][0], current_pos[1] + directions[next_direction][1])
+            queue.append((next_position, next_direction, seen))
+
+    return len(visited)
+
+
+def is_in_grid(grid, pos):
+    row, col = pos
+
+    min_row = min_col = 0
+    max_row = len(grid) - 1
+    max_col = len(grid[0]) - 1
+
+    return not (row < min_row or row > max_row or col < min_col or col > max_col)
+
+
+def get_grid(filename):
+    with open(filename) as file:
+        return [[j.replace('\\', 'b') for j in i.rstrip()] for i in file.readlines()]
+
+
+assert 46 == part1("test/day16.txt")
+st = time.time()
+print("Day 16, Part 1:", part1("input/day16.txt"), " in %s seconds " % (time.time() - st))
+
+assert 51 == part2("test/day16.txt")
+st = time.time()
+print("Day 16, Part 2:", part2("input/day16.txt"), " in %s seconds " % (time.time() - st))
